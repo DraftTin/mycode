2755b0e868951f310080f2044aaff0e08fb4d0d6
flyingbutton <thedrafttin@gmail.com>
2023-10-02 21:49:54 +0800

    add description

--- Floyd.cpp
+++ Floyd.cpp
@@ -8,6 +8,7 @@ using namespace std;
 // 求出每个点和其他点之间最短路径
 // 动态规划：最优子结构 + 重叠子问题: i到j的最短距离取决于，i到其他点k，k再到j的最短距离
 // 时间复杂度O(n^3)
+// 理解：从节点i到节点j的一条不包括编号大于k的中间节点的路径
 void Floyd_(vector<vector<int>>& graph_){
 	for(int k = 0; k < graph_.size(); ++k){
 		for(int i = 0; i < graph_.size(); ++i){
