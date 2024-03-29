22278da99172fbf43e46346e62f9e7f2002834f0
flyingbutton <thedrafttin@gmail.com>
2024-03-29 10:58:09 +0800

    move note file to another project

--- golang_learn/go.mod
+++
@@ -1,3 +0,0 @@
-module example.com/learn
-
-go 1.21.2
--- js_learn/test.js
+++
@@ -1,9 +0,0 @@
-async function myDisplay() {
-  let myPromise = new Promise(function(resolve) {
-    setTimeout(() => resolve("I love You !!"), 3000)
-  });
-  ii = await myPromise;
-  console.log(ii)
-}
-
-myDisplay();
